4678bafdf3d4302fc6b5b15846f5bcc597384126
bavilona119-ship-it <bavilona119@gmail.com>
2025-10-01 15:54:18 +0200

    Update index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,1 +1,17 @@
-import { PageRouteProps } from "@/interface";
+import Button from "@/components/common/Button";
+import { useRouter } from "next/router";
+
+interface PageRouteProps {
+  pageRoute: string;
+}
+
+export default function Home() {
+  const router = useRouter();
+
+  // Imperative routing with useRouter
+  const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
+    router.push(pageRoute, undefined, { shallow: false });
+  };
+
+  return (
+    <div className="
